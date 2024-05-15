d4851bdb58fccbb53d15d3826bb088056489e4ee
TheKelvinT <tnchnkit@hotmail.com>
2024-05-15 09:59:00 +0800

    addd type animation

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import { motion } from "framer-motion";
+import React from "react"
+import { motion } from "framer-motion"
+import { TypeAnimation } from "react-type-animation"
 
-
-  
 const Hero = () => {
   return (
     <section
@@ -10,43 +9,76 @@ const Hero = () => {
       className=" h-screen flex items-center lg:w-[800px]  xl:w-[980px]  py-24 xl:py-36 mx-6 sm:mx-12 md:mx-24  lg:mx-auto lg:flex sm:items-center sm:justify-start   "
     >
       <div>
-    
-      <motion.p initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        transition={{ duration: 0.5, delay:1 }}
-        variants={{hidden: { opacity: 0, y: 50, x:20 },visible: { opacity: 1, y: 0, x:0 }}} 
-        className='text-primary font-spacemono text-sm md:text-base leading-tight'>
-          Hi, my name is 
-      </motion.p>
-        <motion.h1  initial="hidden"
+        <motion.p
+          initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5, delay:1.1  }}
-          variants={{hidden: { opacity: 0, y: -0,x:2 },visible: { opacity: 1, y: 0, x:0 }}} 
-          className='text-4xl md:text-6xl md:leading-normal lg:text-7xl lg:leading-normal font-bold text-light-gray leading-normal'>
-            Kelvin Tan
+          transition={{ duration: 0.5, delay: 1 }}
+          variants={{
+            hidden: { opacity: 0, y: 50, x: 20 },
+            visible: { opacity: 1, y: 0, x: 0 },
+          }}
+          className="text-primary font-spacemono text-sm md:text-base leading-tight"
+        >
+          Hi, my name is
+        </motion.p>
+        <motion.h1
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5, delay: 1.1 }}
+          variants={{
+            hidden: { opacity: 0, y: -0, x: 2 },
+            visible: { opacity: 1, y: 0, x: 0 },
+          }}
+          className="text-4xl md:text-6xl md:leading-normal lg:text-7xl lg:leading-normal font-bold text-light-gray leading-normal"
+        >
+          Kelvin Tan
         </motion.h1>
-        <motion.h2 
+        <motion.h2
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 1 }}
-          transition={{ duration: 0.5, delay:1.2  }}
-          variants={{hidden: { opacity: 0, y: 50,x:2 },visible: { opacity: 1, y: 0, x:0 }}} 
-          className='text-3xl md:text-5xl  md:leading-normal lg:text-7xl lg:leading-tight font-bold leading-tight mb-2'>
-            Transforming web design into reality.
+          transition={{ duration: 0.5, delay: 1.2 }}
+          variants={{
+            hidden: { opacity: 0, y: 50, x: 2 },
+            visible: { opacity: 1, y: 0, x: 0 },
+          }}
+          className="text-3xl md:text-5xl  md:leading-normal lg:text-7xl lg:leading-tight font-bold leading-tight mb-2"
+        >
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              "Transforming web design into reality.",
+              2000, // wait 1s before replacing "Mice" with "Hamsters"
+              "Bridging the Gap Between Design and Development.",
+              2000, // wait 1s before replacing "Mice" with "Hamsters"
+              "Bringing Creative Web Concepts to Life.",
+              2000, // wait 1s before replacing "Mice" with "Hamsters"
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
         </motion.h2>
-        <motion.p 
+        <motion.p
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5, delay:1.3 }}
-          variants={{hidden: { opacity: 0, y: 50,x:2 },visible: { opacity: 1, y: 0, x:0 }}} 
-          className=' text-sm md:text-base max-w-[580px] md:leading-normal leading-normal'>
-            I'm a front-end web developer with a passion for building great online experiences. With a solid foundation in HTML, CSS, JavaScript, and other relevant technologies, I enjoy bringing ideas to life and creating functional and visually appealing websites. Let's make something awesome together!
-            </motion.p>
+          transition={{ duration: 0.5, delay: 1.3 }}
+          variants={{
+            hidden: { opacity: 0, y: 50, x: 2 },
+            visible: { opacity: 1, y: 0, x: 0 },
+          }}
+          className=" text-sm md:text-base max-w-[580px] md:leading-normal leading-normal"
+        >
+          I'm a front-end web developer with a passion for building great online
+          experiences. With a solid foundation in HTML, CSS, JavaScript, and
+          other relevant technologies, I enjoy bringing ideas to life and
+          creating functional and visually appealing websites. Let's make
+          something awesome together!
+        </motion.p>
       </div>
-
     </section>
   )
 }
