14dac363f0348be55445eeb09515e9249de202ae
TheKelvinT <tnchnkit@hotmail.com>
2024-05-15 09:27:19 +0800

    side bar fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,108 @@
-
-import { useState,useEffect } from 'react'
-import Socialbar from './components/SocialBar'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Experience from './pages/Experience'
-import Footer from './pages/Footer'
-import Hero from './pages/Hero'
-import Journey from './pages/Journey'
-import Layout from './pages/Layout'
-import Navbar from './pages/Navbar'
-import Projects from './pages/Projects'
-import Sidebar from './components/Sidebar'
-import {motion} from 'framer-motion'
+import { useState, useEffect } from "react"
+import Socialbar from "./components/SocialBar"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Experience from "./pages/Experience"
+import Footer from "./pages/Footer"
+import Hero from "./pages/Hero"
+import Journey from "./pages/Journey"
+import Layout from "./pages/Layout"
+import Navbar from "./pages/Navbar"
+import Projects from "./pages/Projects"
+import Sidebar from "./components/Sidebar"
+import { motion } from "framer-motion"
 
 const navlinks = [
-  { id: "00", name: "Home", link:"home"},
-  { id: "01", name: "About", link:"about" },
-  { id: "02", name: "Experience", link:"experience" },
-  { id: "03", name: "Projects", link:"projects" },
-  { id: "04", name: "Journey", link:"journey" },
-  { id:"05", name: 'Contact', link:"contact"}
-];
+  { id: "00", name: "Home", link: "home" },
+  { id: "01", name: "About", link: "about" },
+  { id: "02", name: "Experience", link: "experience" },
+  { id: "03", name: "Projects", link: "projects" },
+  { id: "04", name: "Journey", link: "journey" },
+  { id: "05", name: "Contact", link: "contact" },
+]
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home");
-  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const [selectedPage, setSelectedPage] = useState("home")
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
-        setIsTopOfPage(true);
-        setSelectedPage("home");
+        setIsTopOfPage(true)
+        setSelectedPage("home")
       }
-      if (window.scrollY < 100) setIsTopOfPage(false);
-    };
-  
+      if (window.scrollY < 100) setIsTopOfPage(false)
+    }
+
     const sections = [
       { id: "home", elem: document.querySelector("#home") },
       { id: "about", elem: document.querySelector("#about") },
       { id: "experience", elem: document.querySelector("#experience") },
       { id: "projects", elem: document.querySelector("#projects") },
       { id: "journey", elem: document.querySelector("#journey") },
-      { id: "contact", elem: document.querySelector("#contact") }
-    ];
+      { id: "contact", elem: document.querySelector("#contact") },
+    ]
 
-    let observer;
+    let observer
 
     if (window) {
       observer = new IntersectionObserver(
-        entries => {
-          entries.forEach(entry => {
+        (entries) => {
+          entries.forEach((entry) => {
             if (entry.isIntersecting) {
               setSelectedPage(
-                sections.find(section => section.elem === entry.target)
-                  .id
-              );
+                sections.find((section) => section.elem === entry.target).id
+              )
             }
-          });
+          })
         },
-        { threshold: 0.5 }
-      );
+        { threshold: 0.25 }
+      )
 
-      sections.forEach(section => {
-        observer.observe(section.elem);
-      });
+      sections.forEach((section) => {
+        console.log(section)
+        observer.observe(section.elem)
+      })
     }
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll)
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-      if (observer) observer.disconnect();
-    };
-  }, []);
+      window.removeEventListener("scroll", handleScroll)
+      if (observer) observer.disconnect()
+    }
+  }, [])
 
   return (
     <div className="App bg-black-100">
-    <div className='h-screen md:h-full'>
-      <Navbar
-         navlinks={navlinks}
-         selectedPage={selectedPage}
-         setSelectedPage={setSelectedPage}
-         />
-      <Sidebar
-        navlinks={navlinks}
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-      
-      <Socialbar/>
-      {/* <ScrollDownButton
+      <div className="h-screen md:h-full">
+        <Navbar
+          navlinks={navlinks}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+        <Sidebar
+          navlinks={navlinks}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+
+        <Socialbar />
+        {/* <ScrollDownButton
       isTopOfPage={isTopOfPage}/> */}
-      <Hero/>
+        <Hero />
       </div>
- 
-    
-    
-        
-          <About/>
-     
-          <Experience/>
-       
-          <Projects/>
-        
 
+      <About />
 
-          <Journey/>
-       
-        
-          <Contact/>
-      
-      
-      <Footer/>
+      <Experience />
+
+      <Projects />
+
+      <Journey />
+
+      <Contact />
+
+      <Footer />
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,77 +1,86 @@
-import { AnimatePresence, delay, MotionConfig } from "framer-motion";
-import { useState,useEffect, useRef } from "react";
-import AnchorLink from "react-anchor-link-smooth-scroll";
-import Line10px from "./Line10px";
-import { motion } from "framer-motion";
+import { AnimatePresence, delay, MotionConfig } from "framer-motion"
+import { useState, useEffect, useRef } from "react"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import Line10px from "./Line10px"
+import { motion } from "framer-motion"
 
-
-const Sidebar = ({navlinks, selectedPage, setSelectedPage }) => {
+const Sidebar = ({ navlinks, selectedPage, setSelectedPage }) => {
+  console.log(navlinks)
+  console.log(selectedPage)
   const selectedStyles = `text-primary`
-   console.log(selectedPage)
-   
+  console.log(selectedPage)
+
   return (
-    
-    <div className="hidden md:flex flex-col  fixed bottom-[5%] z-50 left-7"  >
+    <div className="hidden md:flex flex-col  fixed bottom-[5%] z-50 left-7">
       {navlinks.map(({ id, name, link, index }) => (
         <motion.div
-        initial="hidden"
+          initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5, delay: 1.7}}
+          transition={{ duration: 0.5, delay: 1.7 }}
           variants={{
             hidden: { opacity: 0, x: -45 },
             visible: { opacity: 1, x: 0 },
           }}
-            >
-         {id !== "00" && (
-          <div className='flex flex-col items-start gap-y-2.5 stroke-light-gray my-[2px] text-sm text-light-gray'>
-          <Line10px/>
-          <Line10px/>
-          <Line10px/>
-          <Line10px/>
+        >
+          {id !== "00" && (
+            <div className="flex flex-col items-start gap-y-2.5 stroke-light-gray my-[2px] text-sm text-light-gray">
+              <Line10px />
+              <Line10px />
+              <Line10px />
+              <Line10px />
+            </div>
+          )}
+          <div className="flex items-center gap-x-2">
+            <AnimatePresence>
+              <motion.span
+                key={id}
+                initial={{ opacity: 0, x: 0 }}
+                animate={{
+                  opacity: 1,
+                  x: selectedPage === name.toLowerCase() ? 0 : -10,
+                }}
+                exit={{
+                  opacity: 0,
+                  x: selectedPage !== name.toLowerCase() ? -20 : 10,
+                }}
+                transition={{ duration: 0.5 }}
+                className={
+                  selectedPage === name.toLowerCase()
+                    ? "stroke-primary"
+                    : "hidden"
+                }
+              >
+                <Line10px />
+              </motion.span>
+            </AnimatePresence>
+            <AnimatePresence>
+              <motion.div
+                initial={{ x: 0 }}
+                animate={{ x: selectedPage === name.toLowerCase() ? 2 : 0 }}
+                exit={{
+                  opacity: 0,
+                  x: selectedPage !== name.toLowerCase() ? -10 : 5,
+                }}
+                transition={{ duration: 0.7 }}
+              >
+                <AnchorLink
+                  key={id}
+                  href={`#${link}`}
+                  className={`${
+                    selectedPage === name.toLowerCase() ? selectedStyles : ""
+                  } font-spacemono hover:text-primary text-sm`}
+                  onClick={() => setSelectedPage(name)}
+                >
+                  {id}
+                </AnchorLink>
+              </motion.div>
+            </AnimatePresence>
           </div>
-          )
-        }
-      <div
-        className="flex items-center gap-x-2"
-       >
-      <AnimatePresence>
-         <motion.span
-         key={id}
-    initial={{ opacity:0, x: 0 }}
-    animate={{ opacity:1, x: selectedPage === name.toLowerCase() ? 0 : -10 }}
-    exit={{ opacity: 0,  x: selectedPage !== name.toLowerCase() ? -20 : 10  }}
-    transition={{ duration:0.5  }}
-    className= {selectedPage===name.toLowerCase()? 'stroke-primary': 'hidden' }
-  >
-    <Line10px />
-  </motion.span>
-  </AnimatePresence>
-  <AnimatePresence>
-    <motion.div
-     initial={{  x: 0}}
-     animate={{ x: selectedPage === name.toLowerCase() ? 2 : 0 }}
-     exit={{ opacity: 0,  x: selectedPage !== name.toLowerCase() ? -10 : 5  }}
-     transition={{ duration: 0.7 }}>
-    <AnchorLink
-    
-    key={id}
-      href={`#${link}`}
-      className={`${
-        selectedPage === name.toLowerCase() ? selectedStyles : ""
-      } font-spacemono hover:text-primary text-sm`}
-      onClick={() => setSelectedPage(name)}
-    >
-     {id}</AnchorLink>
-     </motion.div>
-     </AnimatePresence>
+        </motion.div>
+      ))}
     </div>
-    </motion.div>
- ))}
- 
-  </div>
+  )
+}
 
-  );
-};
-
-export default Sidebar;
+export default Sidebar
